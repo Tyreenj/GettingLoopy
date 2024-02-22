44b03139a5902c4213355d7a49335f4bb533062f
Noah Tyree <noahjtyree@gmail.com>
2024-02-22 14:44:48 -0500

    Second Commit

--- src/CtoF.java
+++ src/CtoF.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class CtoF {
+import java.util.Scanner;
+
+public class CtoF {
+    public static void main(String[] args) {
+
+        //Input
+        Scanner typeIn = new Scanner(System.in);
+        Scanner tempIn = new Scanner(System.in);
+        String trash;
+
+        String type;
+        double temp = 0;
+
+        //C or F Input do while (bulletproofing)
+        boolean typeDone = false;
+        do
+        {
+            System.out.print("Is your temperature in C or F? ");
+            type = typeIn.nextLine();
+            type = type.toUpperCase();
+
+            if(type.equals("C") || type.equals("F"))
+            {
+                typeDone = true;
+
+                //What temp do while input (bulletproofing)
+                boolean tempDone = false;
+                do
+                {
+                    System.out.print("What is your temperature? ");
+
+                    if(tempIn.hasNextDouble())
+                    {
+                        temp = tempIn.nextDouble();
+                        tempDone = true;
+                    } else //Invalid temp catcher
+                    {
+                        trash = tempIn.nextLine();
+                        System.out.print("That is not a valid temperature! " +trash);
+                    }
+
+                } while(!tempDone);
+
+            } else // Invalid type catcher
+            {
+                System.out.println("That is not a valid temperature type! " +type);
+            }
+        } while(!typeDone);
+
+        // Maths for converting temperatures
+        if(type.equals("F"))
+        {
+            temp = (temp - 32) * 5 / 9;
+            System.out.println("Your temperature is "+temp +" degrees Celsius");
+        } else {
+            temp = (temp * 9 / 5) + 32;
+            System.out.println("Your temperature is "+temp +" degrees Fahreneheit!");
+        }
+    }
 }
--- src/GettingLoopy.java
+++ src/GettingLoopy.java
@@ -1,15 +1,92 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class GettingLoopy {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //A loop that counts up by 1 from 0 to 30
+        /*
+        for(int i = 1; i <= 30; i++)
+        {
+            System.out.println(+i);
+        }
+        */
+
+        //A loop that counts down by 1 from 30 to 0
+        /*
+        for(int i = 30; i >= 1; i--)
+        {
+            System.out.println(+i);
+        }
+         */
+
+        //A loop that counts up by 3 from 0 to 18
+        /*
+        for(int i = 0; i <= 18; i += 3)
+        {
+            System.out.println(+i);
+        }
+         */
+
+        //A loop that counts down by 2 from 10 to 0
+        /*
+        for(int i = 10; i >= 0; i -= 2)
+        {
+            System.out.println(+i);
+        }
+         */
+
+        // Task 5
+        /*
+        for(int row = 0; row < 6; row++)
+        {
+            for(int col = 0; col < row; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+         */
+
+        //Task 6
+        /*
+        for(int row = 5; row > 0; row--)
+        {
+            for(int col = 0; col < row; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+         */
+
+        //Task 7
+        /*
+        for(int row = 0; row < 5; row++)
+        {
+            System.out.print("*");
+
+            for(int col = 0; col < 5; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+         */
+
+        //Extra Credit
+        for(int row = 1; row <= 8; row++)
+        {
+            for(int col = 1; col <= 8; col++)
+            {
+                if(col == 1 || col == 8 || col==row)
+                {
+                    System.out.print("*");
+                } else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
 }
